f8e4ba2b77af60736d2a84d883c807cacaad486a
Abel Alebachew <abelalebachewasfaw@gmail.com>
2025-06-05 19:03:04 +0300

    feat: add dark theme

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { ListChecks } from 'lucide-react'; 
+import ThemeToggle from "../Theme-toggle";
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header className="bg-white dark:bg-gray-800 shadow-sm p-4 border-b dark:border-gray-700">
-      <div className="container mx-auto flex items-center">
-        <ListChecks className="h-8 w-8 text-blue-600 dark:text-blue-400 mr-3" />
-        <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
-          Todos Lists
-        </h1>
+    <header className="bg-background text-foreground shadow-sm p-4 border-b dark:border-gray-700">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <img
+            className="w-10 h-auto mr-3"
+            src="/to-do-list.png"
+            alt="Todos Lists Logo"
+          />
+          <h1 className="text-2xl font-bold">
+            Todos Lists
+          </h1>
+        </div>
+        <ThemeToggle />
       </div>
     </header>
   );
